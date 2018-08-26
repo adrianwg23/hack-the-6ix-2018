48b2637a0857e6f84b1b776e65bdcac4ca2f38c0
adrianwg23 <adrianwg23@hotmail.com>
2018-08-25 23:53:15 -0400

    added reserve functionality

--- stdlib-workspace/ritual-offiline/messagehub/functions/messaging/reserve.js
+++ stdlib-workspace/ritual-offiline/messagehub/functions/messaging/reserve.js
@@ -1,22 +1,51 @@
-const lib = require('lib')({ token: process.env.STDLIB_TOKEN })
-const send = require('../../helpers/send.js')
+const lib = require('lib')({
+  token: process.env.STDLIB_TOKEN
+})
+const send = require('../../helpers/send.js');
+const firebase = require("firebase-admin");
+const serviceAccount = require("../../serviceAccountKey.json");
+
+let db = null;
 
 /**
-* MORE handler, responds if user texts "more"
-*  (or any uppercase variation like "MORE")
-* @param {string} sender The phone number that sent the text to be handled
-* @param {string} receiver The StdLib phone number that received the SMS
-* @param {string} message The contents of the SMS
-* @param {string} createdDatetime Datetime when the SMS was sent
-* @returns {any}
-*/
+ * MORE handler, responds if user texts "more"
+ *  (or any uppercase variation like "MORE")
+ * @param {string} sender The phone number that sent the text to be handled
+ * @param {string} receiver The StdLib phone number that received the SMS
+ * @param {string} message The contents of the SMS
+ * @param {string} createdDatetime Datetime when the SMS was sent
+ * @returns {any}
+ */
+
 module.exports = async (sender = '', receiver = '', message = '', createdDatetime = '', context) => {
+  // Initialize firebase app
+  if (!db) {
+    await firebase.initializeApp({
+      credential: firebase.credential.cert(serviceAccount),
+      databaseURL: "https://hack-the-6ix-201-1535217409242.firebaseio.com/"
+    });
+    db = await firebase.database()
+  }
+
+  // Parse message into usable elements
   let parsedMessage = message.split(" #").map((word) => word.trim().replace("#", ""));
-  return send(
-    receiver,
-    sender,
-    `This is the RESERVE handler for your MessageBird SMS handler on StdLib` +
-      `\n\n` +
-      `You can customize its behavior in /functions/messaging/more.js`
-  )
+
+  // Query Firebase
+  let ref = db.ref("restaurants/" + parsedMessage[1] + "/reservations/" + parsedMessage[2] + "/" + parsedMessage[3]);
+  let snapshot = await ref.once("value");
+
+  if (snapshot.val()!= null) {
+    ref.remove();
+    return send(
+      receiver,
+      sender,
+      `You have successfully reserved this time.`
+    );
+  } else {
+    return send(
+      receiver,
+      sender,
+      `The time you requested is not available.`
+    );
+  }
 }
