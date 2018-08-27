99086e574d2a5020509c1408616053864bab41f3
enochtangg <enochtangg@gmail.com>
2018-08-26 20:15:28 -0400

    change

--- stdlib-workspace/ritual-offiline/messagehub/functions/messaging/order.js
+++ stdlib-workspace/ritual-offiline/messagehub/functions/messaging/order.js
@@ -67,7 +67,7 @@ module.exports = async (sender = '', receiver = '', message = '', createdDatetim
       order: menu_item
     }
 
-    let res = await axios.post('https://5bbee879.ngrok.io/send_order', data)
+    let res = await axios.post('https://98b5e5d7.ngrok.io/send_order', data)
     console.log(res.data);
 
     return send(
